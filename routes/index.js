03a3cd31e942882622990ff1bcd3a3be33f70a6a
王卓 <280034816@qq.com>
2016-08-14 00:41:17 +0800

    test push

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '星尘盒子for300英雄  Powered by Express' });
 });
 
-router.get('/equipment', function(req, res) {
+router.get('/300Hero/API/equipment', function(req, res) {
     var equipment=JSON.parse(fs.readFileSync('./equipment.json'));
     res.json(equipment);
 });
 
-router.get('/hero', function(req, res) {
+router.get('/300Hero/API//hero', function(req, res) {
     var hero=JSON.parse(fs.readFileSync('./hero.json'));
     res.json(hero);
 });
